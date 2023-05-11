88052b04db0d511b8818ac4299d28c22b95e82f4
= <=>
2023-05-12 01:06:30 +0700

    transfer database to cloud

--- src/common/database.js
+++ src/common/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const db = () => {
-    mongoose.connect('mongodb://0.0.0.0:27017/tech_shop', {
+    mongoose.connect('mongodb+srv://nguyenthenamuet:112233As@cluster0.9q78hpl.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
